ecf4732cd9c081978cb14c6b0ec0a28c95d9ea95
Caleb <kamenjuhkiiru@gmail.com>
2023-09-16 10:26:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const surprise = "surprise"
+const thankYou = [];
+
+function writeCards(names, surprise) {
+    for (let i = 0; i < names.length; i++) {
+        const msg = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`;
+        thankYou.push(msg)
+        debugger;
+    }
+    return thankYou
+}
+
+function countDown() {
+    let count = 10
+    while(count >= 0) {
+      console.log(count--)
+    }
+  }
+
+countDown()
